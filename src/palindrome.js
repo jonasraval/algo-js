a9cc27aeae85d071b1189702f08d9b88793c8620
Boris Fritscher <boris@fritscher.ch>
2018-08-08 16:52:06 +0200

    fixed wrong assignment text for palindrome

--- src/palindrome.js
+++ src/palindrome.js
@@ -1,9 +1,12 @@
 //@ts-check
 
 /*
-Write a function that splits an array (first argument) into groups
-the length of size (second argument) and returns them as
-a two-dimensional array.
+Return true if the given string is a palindrome. Otherwise, return false.
+
+A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+Note
+You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
 */
 
 function palindrome(str) {
